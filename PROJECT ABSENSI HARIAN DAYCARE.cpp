ca45325af71cbe6c01c10dc5b7d18beb09e8f166
Sumems <111702142+Sumems@users.noreply.github.com>
2022-11-29 07:50:05 +0700

    Add files via upload

---
+++ PROJECT ABSENSI HARIAN DAYCARE.cpp
@@ -0,0 +1,149 @@
+/*PROJECT DASPRO DAN PRPL
+Studi Kasus Absensi Harian DayCare*/
+  
+#include<iostream>
+using namespace std;
+
+//////////////////// PAGE PENENTUAN NOMOR IDENTITAS ANAK SAAT PENDAFTARAN /////////////////////////////
+/*string nomoridentitas(){
+	
+} */
+///////////////// MENU 1 PENDAFTARAN ANAK //////////////////////////
+void pendaftaran(){
+	string namaortu, namaanak, alergipenyakit, pekerjaanortu, alamatasal, alamatsaatini, alamatkantor;
+	int usia;
+	
+	cout<<" \t\t\t\t========================= "<<endl;
+	cout<<" \t\t\t\t====== PENDAFTARAN ====== "<<endl;
+	cout<<" \t\t\t\t======== DAYCARE ======== "<<endl;
+	cout<<" \t\t\t\t========================= "<<endl;
+	cout<<endl;
+	cout<<" \t\t\t\tMasukkan Nama Orang Tua      : ";cin>>namaortu;getline(cin, namaortu);
+	cout<<" \t\t\t\t\n Masukkan Nama Anak           : ";cin>>namaanak;getline(cin, namaanak);
+	cout<<" \t\t\t\t\n Masukkan Usia Anak           : ";cin>>usia;
+	cout<<" \t\t\t\t\n Riwayat Penyakit/Alergi Anak : ";cin>>alergipenyakit;getline(cin, alergipenyakit);
+	cout<<" \t\t\t\t\n Pekerjaan Orang Tua          : ";cin>>pekerjaanortu;getline(cin, pekerjaanortu);
+	cout<<" \t\t\t\t\n Alamat Asal                  : ";cin>>alamatasal;getline(cin, alamatasal);
+	cout<<" \t\t\t\t\n Alamat Saat Ini              : ";cin>>alamatsaatini;getline(cin, alamatsaatini);
+	cout<<" \t\t\t\t\n Alamat Kantor                : ";cin>>alamatkantor;getline(cin, alamatkantor);
+	cout<<endl;
+	cout<<" Nama: "<<namaanak<<", Usia: "<<usia<<", Riwayat Penyakit/Alergi: "<<alergipenyakit<<endl;
+}
+//////////////// MENU 2 ABSENSI KEDATANGAN ANAK ////////////////////
+void absensidatang(){
+	
+}
+//////////////// MENU 3 ABSENSI KEPULANGAN ANAK ///////////////////
+void absensipulang(){
+	
+}
+//////////////// MENU 4 LAPORAN ABSENSI SELURUH ANAK DI DAYCARE ////////////////////////
+void laporanabsensi(){
+	
+}
+////////////// DAFTAR MENU UTAMA //////////////////////////
+int menu(){
+	string ulang;
+	int menu;
+	
+	do{
+		system("cls");
+		cout<<" ==================="<<endl;
+		cout<<" ====== MENU ======="<<endl;
+		cout<<" ==================="<<endl;
+		cout<<endl;
+		cout<<" 1. Pendaftaran "<<endl;
+		cout<<" 2. Absensi Datang "<<endl;
+		cout<<" 3. Absensi Pulang "<<endl;
+		cout<<" 4. Laporan Absensi "<<endl;
+		cout<<" \n Pilih Menu: ";cin>>menu;
+		
+		system("cls");
+		switch(menu){
+			case 1:
+				system("cls");
+				cout<<endl;
+				pendaftaran();
+			break;
+			case 2:
+				system("cls");
+				cout<<endl;
+				absensidatang();
+			break;
+			case 3:
+				system("cls");
+				cout<<endl;
+				absensipulang;
+			break;
+			case 4:
+				system("cls");
+				cout<<endl;
+				laporanabsensi();
+			break;
+		default:
+			cout<<" ERROR "<<endl;
+		}
+		cout<<" \n Ulangi? <y/n> ";cin>>ulang;
+	}while(ulang == "y");
+	cout<<endl;
+	system("pause");
+	
+	return(menu);
+}
+//////////// PAGE LOGIN PETUGAS /////////////////
+void login(){
+	string username, realusername, pass, realpass, kembali;
+	
+	
+	for(int i = 2; i >= 0; --i){
+		system("cls");
+		cout<<" ===================="<<endl;
+		cout<<" ====== LOGIN ======="<<endl;
+		cout<<" ===================="<<endl;
+		cout<<endl;
+		cout<<" Masukkan Username : ";cin>>username;
+		cout<<" Masukkan Password : ";cin>>pass;
+		cout<<endl;
+		
+		realusername = "dutafirdaus";
+		realpass = "dutafirdausterbaik";
+		
+		if(username == realusername && pass == realpass){
+			do{
+				cout<<endl;
+				menu();
+				system("cls");
+				cout<<" \n Kembali Ke Menu? <y/n> ";cin>>kembali;
+			}while(kembali == "y");
+			cout<<endl;
+			system("pause");
+			break;
+		}else if(i > 0){
+			cout<<" Password atau Username yang Anda Masukkan Salah. "<<endl;
+			cout<<" Kesempatan Anda "<<i<<" Kali Lagi. "<<endl;
+			cout<<endl;
+			system("pause");
+		}else{
+			cout<<" ANDA TIDAK DIPERKENANKAN UNTUK MENGAKSES APLIKASI INI! "<<endl;
+			break;
+		}
+	}
+}
+/////////////// MAIN FUNCITON ////////////////////
+int main(){
+	string logout;
+	
+	do{
+		cout<<endl;
+		system("cls");
+		login();
+		cout<<" \n Logout atau ulangi masukkan usename dan password? <y/n> ";cin>>logout;
+	}while(logout == "y");
+	cout<<endl;
+	system("cls");
+	cout<<" ================================="<<endl;
+	cout<<" = Program Selesai, Terima Kasih.= "<<endl;
+	cout<<" ================================="<<endl;
+	
+	return 0;
+}
